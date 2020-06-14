d7988df59410216ed216c7621225e0ae7cc9f14c
StuartCox3107 <coxstuart@hotmail.com>
2020-06-14 20:38:42 +0000

    jquery color change added for progress bar on hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,3 +11,13 @@ $(".btn").on("mouseleave", function() {
   $(this).css("background-color", "rgb(148, 140, 139)");
 });
 
+
+//jquery mouse over/leave to change the background color of the progress bar
+$(".progress-bar").on("mouseover", function() {
+  $(this).css("background-color", "#52796f");
+});
+
+$(".progress-bar").on("mouseleave", function() {
+  $(this).css("background-color", "rgb(148, 140, 139)");
+});
+
